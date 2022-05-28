0af001420a96fc59eb7f0e7176f6710d41c2b180
Kittisak Phatchaiphongsa <kittisak.ph2001@gmail.com>
2022-05-28 22:20:52 +0700

    Structure

---
+++ lib/screen/home.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Register / Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: Column(children: [
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text(
+                "Sign up",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.login),
+              label: Text(
+                "Sign in",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
